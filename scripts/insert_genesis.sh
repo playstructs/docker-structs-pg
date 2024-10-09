c3b9c7f0aebb1137b369c21f2504e16a9c266a03
abstrct <abstrct@slow.ninja>
2024-10-09 14:19:28 -0400

    jq! we're missin some ya'-r matey

--- scripts/insert_genesis.sh
+++ scripts/insert_genesis.sh
@@ -13,19 +13,19 @@ ALL_GENESIS_BLOB=`cat structs-networks/genesis.json`
 ALL_GENESIS_COUNT=`echo ${ALL_GENESIS_BLOB} | jq ".app_state.bank.balances" | jq length `
 echo "Found ${ALL_GENESIS_COUNT} Genesis records"
 
-ALL_GENESIS_TIME=`echo ${ALL_GENESIS_BLOB} | jq ".genesis_time" `
+ALL_GENESIS_TIME=`echo ${ALL_GENESIS_BLOB} | jq -r ".genesis_time" `
 
 for (( p=0; p<ALL_GENESIS_COUNT; p++ ))
 do
-  GENESIS_ADDRESS=`echo ${ALL_GENESIS_BLOB} | jq ".app_state.bank.balances[${p}].address"`
+  GENESIS_ADDRESS=`echo ${ALL_GENESIS_BLOB} | jq -r ".app_state.bank.balances[${p}].address"`
 
-  GENESIS_COUNT=`echo ${ALL_GENESIS_BLOB} | jq ".app_state.bank.balances[${p}].coins" | jq length `
+  GENESIS_COUNT=`echo ${ALL_GENESIS_BLOB} | jq -r ".app_state.bank.balances[${p}].coins" | jq length `
 
   for (( c=0; c<GENESIS_COUNT; c++ ))
   do
 
-    GENESIS_COIN_DENOM=`echo ${ALL_GENESIS_BLOB} | jq ".app_state.bank.balances[${p}].coins[${c}].denom"`
-    GENESIS_COIN_AMOUNT=`echo ${ALL_GENESIS_BLOB} | jq ".app_state.bank.balances[${p}].coins[${c}].amount"`
+    GENESIS_COIN_DENOM=`echo ${ALL_GENESIS_BLOB} | jq -r ".app_state.bank.balances[${p}].coins[${c}].denom"`
+    GENESIS_COIN_AMOUNT=`echo ${ALL_GENESIS_BLOB} | jq -r ".app_state.bank.balances[${p}].coins[${c}].amount"`
 
     QUERY="INSERT INTO structs.ledger(address, amount, block_height, updated_at, created_at, action, direction, denom) VALUES('${GENESIS_ADDRESS}','${GENESIS_COIN_AMOUNT}', 0, NOW(), '${ALL_GENESIS_TIME}', 'genesis', 'credit', '${GENESIS_COIN_DENOM}'); "
 
